099aa985fd792c6e3a76c0469cf9071f289d887b
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2019-02-15 13:16:37 -0400

    Configurando las variables de entorno y concatenando la url de la conexión con las variables de entorno

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
-const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, PORT, DB_HOST } = process.env;
 
-const sequelize = new Sequelize('mysql://uozmhg1r06aeugqmljcs:2oXvBJg2epxmdNX0GoIa@b2iacgn3r4tgjzrkdsej-mysql.services.clever-cloud.com:3306/b2iacgn3r4tgjzrkdsej', {
-  //                            dialect -----user------------ ----pass------------ -------host----------------------------------------- port ---database name ----
+const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/${DB_NAME}`, {
+  //                            dialect -----user---------pass---------host----- port------database name
   pool: {
     max: 5,
     min: 0,
