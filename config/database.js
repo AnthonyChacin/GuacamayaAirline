f7999f2c4168c5cb38ecca29de7274893d6f4ce0
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2019-02-15 09:55:33 -0400

    Configuración Clevercloud

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
 const sequelize = new Sequelize('mysql://uozmhg1r06aeugqmljcs:2oXvBJg2epxmdNX0GoIa@b2iacgn3r4tgjzrkdsej-mysql.services.clever-cloud.com:3306/b2iacgn3r4tgjzrkdsej', {
+  //                            dialect -----user------------ ----pass------------ -------host----------------------------------------- port ---database name ----
   pool: {
     max: 5,
     min: 0,
